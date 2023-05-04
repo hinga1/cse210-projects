b5e323bd3b5c7351c46bb31c87f8bab2bf9f2057
hinga1 <kam22009@byui.edu>
2023-05-04 18:46:16 +0300

    finished with prep 2,3,4,5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 101);
+        int guesses = 0;
+        int guess;
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess what it is?");
+        do
+        {
+            Console.Write("Enter your guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            guesses++;
+
+            if (guess < numberToGuess)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > numberToGuess)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number in {0} guesses.", guesses);
+                break;
+            }
+        } while (true);
+
+        Console.Write("Do you want to play again? (Y/N): ");
+        string answer = Console.ReadLine();
+
+        if (answer == "Y" || answer == "y")
+        {
+            Main(args);
+        }
+        else
+        {
+            Console.WriteLine("Thanks for playing!");
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        double favNumber = double.Parse(Console.ReadLine());
+
+        double favNumberSqrt = Math.Sqrt(favNumber);
+
+        Console.WriteLine($"Brother {name}, the square root of your favorite number is {favNumberSqrt}");
     }
 }
