8a26e81e26debc59e20cf403cfcdf0cf837a7b07
hinga1 <kam22009@byui.edu>
2023-07-08 16:26:49 +0300

    added foundation 1 and 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,137 @@
 using System;
+using System.Collections.Generic;
+
+// Base class Video
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+}
+
+// Subclass Comment inheriting from Video
+class Comment : Video
+{
+    public string CommenterName { get; set; }
+    public string CommentText { get; set; }
+}
+
+// YouTubeVideoManager class to manage videos and comments
+class YouTubeVideoManager
+{
+    private List<Video> videos;
+    private List<Comment> comments;
+
+    public YouTubeVideoManager()
+    {
+        videos = new List<Video>();
+        comments = new List<Comment>();
+    }
+
+    public void AddVideo(Video video)
+    {
+        videos.Add(video);
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount(Video video)
+    {
+        int count = 0;
+        foreach (Comment comment in comments)
+        {
+            if (comment.Title == video.Title && comment.Author == video.Author && comment.Length == video.Length)
+                count++;
+        }
+        return count;
+    }
+
+    public void DisplayVideoInfoWithComments(Video video)
+    {
+        Console.WriteLine("Video Title: " + video.Title);
+        Console.WriteLine("Author: " + video.Author);
+        Console.WriteLine("Length: " + video.Length + " minutes");
+
+        Console.WriteLine("Comments:");
+        foreach (Comment comment in comments)
+        {
+            if (comment.Title == video.Title && comment.Author == video.Author && comment.Length == video.Length)
+            {
+                Console.WriteLine("Commenter: " + comment.CommenterName);
+                Console.WriteLine("Comment: " + comment.CommentText);
+                Console.WriteLine();
+            }
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        YouTubeVideoManager videoManager = new YouTubeVideoManager();
+
+        // Creating videos
+        Video video1 = new Video
+        {
+            Title = "Video 1",
+            Author = "Author 1",
+            Length = 5
+        };
+
+        Video video2 = new Video
+        {
+            Title = "Video 2",
+            Author = "Author 2",
+            Length = 10
+        };
+
+        // Adding videos
+        videoManager.AddVideo(video1);
+        videoManager.AddVideo(video2);
+
+        // Adding comments
+        Comment comment1 = new Comment
+        {
+            Title = "Video 1",
+            Author = "Author 1",
+            Length = 5,
+            CommenterName = "User 1",
+            CommentText = "Great video!"
+        };
+
+        Comment comment2 = new Comment
+        {
+            Title = "Video 1",
+            Author = "Author 1",
+            Length = 5,
+            CommenterName = "User 2",
+            CommentText = "Nice content!"
+        };
+
+        Comment comment3 = new Comment
+        {
+            Title = "Video 2",
+            Author = "Author 2",
+            Length = 10,
+            CommenterName = "User 3",
+            CommentText = "Very informative!"
+        };
+
+        videoManager.AddComment(comment1);
+        videoManager.AddComment(comment2);
+        videoManager.AddComment(comment3);
+
+        // Retrieving comment count
+        int commentCount = videoManager.GetCommentCount(video1);
+        Console.WriteLine("Comment count for Video 1: " + commentCount);
+
+        // Displaying video info with comments
+        videoManager.DisplayVideoInfoWithComments(video1);
+
+        Console.ReadLine();
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,131 @@
 using System;
+using System.Collections.Generic;
+
+// Product class
+class Product
+{
+    public string Name { get; set; }
+    public int ProductID { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
+
+// Address class
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string StateProvince { get; set; }
+    public string Country { get; set; }
+}
+
+// Customer class
+class Customer
+{
+    public string Name { get; set; }
+    public Address Address { get; set; }
+
+    public bool IsInUSA()
+    {
+        return Address.Country == "USA";
+    }
+}
+
+// Order class
+class Order
+{
+    public List<Product> Products { get; set; }
+    public Customer Customer { get; set; }
+
+    public decimal CalculateTotalPrice()
+    {
+        decimal totalPrice = 0;
+        foreach (Product product in Products)
+        {
+            totalPrice += product.Price * product.Quantity;
+        }
+        return totalPrice;
+    }
+
+    public string GeneratePackingLabel()
+    {
+        string packingLabel = "";
+        packingLabel += "Customer: " + Customer.Name + Environment.NewLine;
+        packingLabel += "Shipping Address: " + Customer.Address.Street + ", " + Customer.Address.City + ", " + Customer.Address.StateProvince + ", " + Customer.Address.Country + Environment.NewLine;
+        packingLabel += "Products:" + Environment.NewLine;
+        foreach (Product product in Products)
+        {
+            packingLabel += "- " + product.Name + " (ID: " + product.ProductID + ")" + Environment.NewLine;
+        }
+        return packingLabel;
+    }
+
+    public string GenerateShippingLabel()
+    {
+        string shippingLabel = "";
+        shippingLabel += "Shipping Address: " + Customer.Address.Street + ", " + Customer.Address.City + ", " + Customer.Address.StateProvince + ", " + Customer.Address.Country;
+        return shippingLabel;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Creating products
+        Product product1 = new Product
+        {
+            Name = "Product 1",
+            ProductID = 1,
+            Price = 10,
+            Quantity = 2
+        };
+
+        Product product2 = new Product
+        {
+            Name = "Product 2",
+            ProductID = 2,
+            Price = 5,
+            Quantity = 3
+        };
+
+        // Creating address
+        Address address = new Address
+        {
+            Street = "123 Main Street",
+            City = "New York",
+            StateProvince = "NY",
+            Country = "USA"
+        };
+
+        // Creating customer
+        Customer customer = new Customer
+        {
+            Name = "John Doe",
+            Address = address
+        };
+
+        // Creating order
+        Order order = new Order
+        {
+            Products = new List<Product> { product1, product2 },
+            Customer = customer
+        };
+
+        // Generating packing label
+        string packingLabel = order.GeneratePackingLabel();
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(packingLabel);
+
+        // Generating shipping label
+        string shippingLabel = order.GenerateShippingLabel();
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(shippingLabel);
+
+        // Calculating total price
+        decimal totalPrice = order.CalculateTotalPrice();
+        Console.WriteLine("Total Price: $" + totalPrice);
+
+        Console.ReadLine();
     }
 }
